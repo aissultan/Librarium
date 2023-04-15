0b40dadc7a6b9158b2ea38060f64d33a587e35bb
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-15 16:43:32 +0600

    Create sign-in page

--- Librarium/src/app/app-routing.module.ts
+++ Librarium/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"sign-in",component :SignInComponent},
+  {path:'password',component : ForgotPasswordComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Librarium/src/app/app.module.ts
+++ Librarium/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    ForgotPasswordComponent,
+ 
   ],
   imports: [
     BrowserModule,
