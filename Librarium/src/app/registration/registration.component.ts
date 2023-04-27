a25d87f2c2a9120db2525aec7da477650d9540cc
Temirlan Umerbekov <t_umerbekov@kbtu.kz>
2023-04-28 00:29:22 +0600

    Improving Sign-up

--- Librarium/src/app/registration/registration.component.ts
+++ Librarium/src/app/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -12,26 +14,39 @@ export class RegistrationComponent {
   username: string = '';
   password: string = '';
   confirmPassword: string = '';
-  logged : boolean = false ;
 
-  constructor(private http: HttpClient,private loginService : LoginService) {}
-
-  register(){
-    if  (this.password==this.confirmPassword){
-      this.registerUser(this.email,this.username,this.password)
-    }
-    else {
-      window.alert("Password are not the same")
+  constructor(private http: HttpClient,private router: Router) {}
+  ngOnInit(){
+    this.email = '';
+    this.username = '';
+    this.password = '';
+    this.confirmPassword = '';
+  }
+  onSubmit() {
+    if (this.email === '' || this.username === '' || this.password === '' || this.confirmPassword === '') {
+      window.alert('Please fill in all required fields.');
+    } else if (this.password !== this.confirmPassword) {
+      window.alert('Passwords do not match.');
+    } else {
+      this.registerUser(this.email, this.username, this.password);
     }
   }
   registerUser(email : string, username : string,password :string) {
     this.http.post('http://127.0.0.1:8000/api/register/',{email,username,password}).subscribe(
         (response) => {
           console.log('Registration successful.');
-          this.logged = true;
+          email = '';
+          username = '';
+          password = '';
+          this.confirmPassword = '';
+          this.router.navigate(['/sign-in'])
         },
         (error) => {
           console.log('Registration failed.');
+          email = '';
+          username = '';
+          password = '';
+          this.confirmPassword = '';
         }
       );
   }
